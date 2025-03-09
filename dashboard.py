72dc2446f6546da3e5562d8b126610f39e9de478
Baptiste Kyritsos <94804552+bapkyr@users.noreply.github.com>
2025-03-09 18:22:40 +0100

    truc

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,6 @@ from db import get_expenses_by_category, get_expenses_grouped_by_date
 
 def dashboard_page(page):
     data = get_expenses_by_category()
-    data_mth = get_expenses_grouped_by_date()
-
+    categories = [item[0] for item in data]
+    totals = [item[1] for item in data]
     
